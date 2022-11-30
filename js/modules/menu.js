070dfa67405b162d97f5c24593432c2696667496
Mateus <mateus.silvainfo@gmail.com>
2022-11-29 21:16:42 -0300

    added smooth scroll

--- js/modules/menu.js
+++ js/modules/menu.js
@@ -1,8 +1,17 @@
 export default function iniciarMenu() {
     let btnMenu = document.querySelector('#btn-menu')
     let nav = document.querySelector('nav')
+    let li = nav.querySelectorAll('li')
 
     btnMenu.addEventListener('click', () => {
         nav.classList.toggle('visivel')
+    })
+
+    li.forEach(item => {
+        item.addEventListener('click', () => {
+            if (window.innerWidth <= 800) {
+                nav.classList.toggle('visivel')
+            }
+        })
     })
 }
